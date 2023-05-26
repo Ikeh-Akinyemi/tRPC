f501b73dd6173f36f442a83bc5614b5c9c329264
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-05-26 09:05:01 +0100

    ft: complete frontend UI and functions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import type { TRPCRouter } from '../../server/src/router';
+import { createReactQueryHooks } from '@trpc/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Create from './cats/Create';
+import Detail from './cats/Detail';
+import List from './cats/List';
+
+const BACKEND_URL: string = "http://localhost:8080/cat";
+
+export const trpc = createReactQueryHooks<TRPCRouter>();
 
 function App() {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() => trpc.createClient({ url: BACKEND_URL }));
+
+  const [detailId, setDetailId] = useState(-1);
+
+  const setDetail = (id: number) => {
+    setDetailId(id);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <Create />
+          <List setDetail={setDetail}/>
+          { detailId > 0 ? <Detail id={detailId} /> : null }
+        </div>
+      </QueryClientProvider>
+    </trpc.Provider>
   );
 }
 
