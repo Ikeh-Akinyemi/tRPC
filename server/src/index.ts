2ee4ad737f89394a0d5e7fe07224d97ce812eaaf
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-05-26 07:59:34 +0100

    ft: implement express server using tRPC as adapter

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+
+import express, { Application } from 'express';
+import cors from 'cors';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import trpcRouter from './router';
+
+const app: Application = express();
+
+const createContext = ({}: trpcExpress.CreateExpressContextOptions) => ({})
+
+app.use(express.json());
+app.use(cors());
+app.use(
+    '/cat',
+    trpcExpress.createExpressMiddleware({
+        router: trpcRouter,
+        createContext,
+    }),
+);
+
+app.listen(8080, () => {
+    console.log("Server running on port 8080");
+});
